c64de268865fa58dd23eb7e363104adab5f4edf9
Swarnim Srijan <1142srijan2002@gmail.com>
2023-11-04 00:19:09 +0530

    Update Coin_change.cpp

--- Dynamic Programming/Problems/Medium/Coin_change.cpp
+++ Dynamic Programming/Problems/Medium/Coin_change.cpp
@@ -1,6 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+//1st approach
+//Recursive approach
+//Tc- O(2^n), Sc - O()
+
 int coin_count(vector<int>& coins, int n, int amount){
     if(amount == 0) return 0;
     int ans = INT_MAX;
@@ -15,6 +20,28 @@ int coin_count(vector<int>& coins, int n, int amount){
     return (ans == INT_MAX) ? -1 : ans;
 }
 
+
+//2nd approach
+//Dp approach(Tabulation)
+//
+int coin_count(vector<int>& coins, int n, int amount){
+    intdp[amount + 1];
+    dp[0] = 0;
+    for(int i = 1; i <= amount; i++){
+        dp[i] = INT_MAX;
+    }
+    for(int i = 1; i <= amount; i++){
+        for(int j = 0; j < n; j++){
+            if(coins[j] <= i){
+                int get_res = dp[i - coins[j]];
+                if(get_res != INT_MAX){
+                    dp[i] = min(dp[i], get_res + 1);
+                }
+            }
+        }
+    }
+    return (dp[amount] == INT_MAX) ? -1 : dp[amount];
+}  
 
 int main(){
     int amount, n;
