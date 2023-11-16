c8dc97e74aeaab5a1c31119be217dafc688f8a3c
Swarnim Srijan <1142srijan2002@gmail.com>
2023-11-16 17:03:36 +0530

    Create Search_insert_position.cpp

---
+++ Arrays/Problems/Easy/Search_insert_position.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) low = mid + 1;
+            else{
+                high = mid - 1;
+            }
+        }
+        return high + 1;
+    }
+};
+
+/*
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        // find the lower bound of the target in nums
+        
+        int idx = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        
+        return idx;
+    }
+}
+*/
